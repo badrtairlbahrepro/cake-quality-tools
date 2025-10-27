62f10b1190c82d2c8d8290deddca1ea4ba50d75c
badr <badr@north-star.network>
2025-10-27 07:15:44 +0100

    Add quality prefix to routes to avoid conflicts

--- config/routes.php
+++ config/routes.php
@@ -8,8 +8,10 @@ declare(strict_types=1);
 use Cake\Routing\RouteBuilder;
 
 return function (RouteBuilder $builder) {
-    $builder->connect('/', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'index']);
-    $builder->connect('/run-tests', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runTests']);
-    $builder->connect('/run-stan', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runStan']);
-    $builder->connect('/run-cs', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runCodeSniffer']);
+    $builder->prefix('quality', function (RouteBuilder $builder) {
+        $builder->connect('/', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'index']);
+        $builder->connect('/run-tests', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runTests']);
+        $builder->connect('/run-stan', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runStan']);
+        $builder->connect('/run-cs', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runCodeSniffer']);
+    });
 };
