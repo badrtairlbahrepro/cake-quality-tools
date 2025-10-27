0d63d0c514aaa9fd49deab2f1c89b03fc8416f04
badr <badr@north-star.network>
2025-10-27 07:12:14 +0100

    Fix routes to specify plugin namespace

--- config/routes.php
+++ config/routes.php
@@ -8,8 +8,8 @@ declare(strict_types=1);
 use Cake\Routing\RouteBuilder;
 
 return function (RouteBuilder $builder) {
-    $builder->connect('/', ['controller' => 'QualityTools', 'action' => 'index']);
-    $builder->connect('/run-tests', ['controller' => 'QualityTools', 'action' => 'runTests']);
-    $builder->connect('/run-stan', ['controller' => 'QualityTools', 'action' => 'runStan']);
-    $builder->connect('/run-cs', ['controller' => 'QualityTools', 'action' => 'runCodeSniffer']);
+    $builder->connect('/', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'index']);
+    $builder->connect('/run-tests', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runTests']);
+    $builder->connect('/run-stan', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runStan']);
+    $builder->connect('/run-cs', ['plugin' => 'CakeQualityTools', 'controller' => 'QualityTools', 'action' => 'runCodeSniffer']);
 };
